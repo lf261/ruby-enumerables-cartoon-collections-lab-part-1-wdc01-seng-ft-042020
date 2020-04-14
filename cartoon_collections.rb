57a56dcb74f184b04affe00c04948cc4d3edf151
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-14 16:04:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,8 @@ def greet_characters(array)
   total = 0 
   
   array.each do |element|
+    puts "#{element}"
+  end 
   
 end
 
@@ -13,4 +15,6 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  
+  
 end
